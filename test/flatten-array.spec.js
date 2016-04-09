2b5ea73269b978702a3bc7706a9a6815af380fad
Fagner Brack <github@fagnermartins.com>
2016-04-09 13:20:25 +1000

    Create a test for null argument

--- test/flatten-array.spec.js
+++ test/flatten-array.spec.js
@@ -5,6 +5,7 @@ var flattenArray = require("../src/flatten-array");
 describe("flatten-array", function() {
   it("should always return an array despite the argument", function() {
     expect(flattenArray()).to.be.an(Array);
+    expect(flattenArray(null)).to.be.an(Array);
     expect(flattenArray(4)).to.be.an(Array);
     expect(flattenArray({})).to.be.an(Array);
     expect(flattenArray("")).to.be.an(Array);
